623383d0c3955832511239006767451a90422e94
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-23 16:51:01 -0300

    quase la

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
-import ListaCategoria from './components/categorias/listaCategorias/ListaCategorias';
-import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
-
-
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaCategoria from "./components/categorias/listaCategorias/ListaCategorias";
+import FormularioCategoria from "./components/categorias/formularioCategoria/FormularioCategoria";
 
 function App() {
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/categorias" element={<ListaCategoria/>} />
-              <Route path="/categorias" element={<FormularioCategoria/>} />
-              <Route path="/cadastrarCategorias" element={<FormularioCategoria/>} />
-              
+              <Route path="/categorias" element={<ListaCategoria />} />
+              <Route path="/categoriasnovo" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />}/>
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
   );
 }
--- src/components/categorias/formularioCategoria/FormularioCategoria.tsx
+++ src/components/categorias/formularioCategoria/FormularioCategoria.tsx
@@ -1,10 +1,9 @@
 //import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
-import Categoria from '../../../models/Categoria';
-import { atualizar, buscar, cadastrar } from '../../../services/Service';
-import { ChangeEvent, useContext, useEffect, useState } from 'react';
-
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import Categoria from "../../../models/Categoria";
+import { atualizar, buscar, cadastrar } from "../../../services/Service";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 
 function FormularioCategoria() {
   const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
@@ -12,13 +11,13 @@ function FormularioCategoria() {
   const navigate = useNavigate();
 
   const { id } = useParams<{ id: string }>();
-  const categoriaId = parseInt(id ?? '')
+  const categoriaId = parseInt(id ?? "");
 
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
   async function buscarPorId(id: number) {
-    const eString= id.toString()
+    const eString = id.toString();
     await buscar(`/categorias/${eString}`, setCategoria, {
       headers: {
         Authorization: token,
@@ -27,93 +26,90 @@ function FormularioCategoria() {
   }
 
   useEffect(() => {
-    if (categoriaId  !== undefined) {
-      buscarPorId(categoriaId)
+    if (categoriaId !== undefined) {
+      buscarPorId(categoriaId);
     }
-  }, [categoriaId])
+  }, [categoriaId]);
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setCategoria({
       ...categoria,
-      id:categoriaId,
-      [e.target.name]: e.target.value
-    })
+      id: categoriaId,
+      [e.target.name]: e.target.value,
+    });
 
-    console.log(JSON.stringify(categoria))
+    console.log(JSON.stringify(categoria));
   }
 
   async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
-    e.preventDefault()
+    e.preventDefault();
 
     if (categoriaId !== undefined) {
       try {
         await atualizar(`/categorias`, categoria, setCategoria, {
           headers: {
-            'Authorization': token
-          }
-        })
+            Authorization: token,
+          },
+        });
 
-        alert('Categoria atualizada com sucesso') 
-        retornar()
-
+        alert("Categoria atualizada com sucesso");
+        retornar();
       } catch (error: any) {
-        if (error.toString().includes('403')) {
-          alert('O token expirou, favor logar novamente')
-          handleLogout()
+        if (error.toString().includes("403")) {
+          alert("O token expirou, favor logar novamente");
+          handleLogout();
         } else {
-          alert('Erro ao atualizar a Categoria')
+          alert("Erro ao atualizar a Categoria");
         }
-
       }
-
     } else {
       try {
         await cadastrar(`/categorias`, categoria, setCategoria, {
           headers: {
-            'Authorization': token
-          }
-        })
+            Authorization: token,
+          },
+        });
 
-        alert('Categoria cadastrada com sucesso')
-
+        alert("Categoria cadastrada com sucesso");
       } catch (error: any) {
-        if (error.toString().includes('403')) {
-          alert('O token expirou, favor logar novamente')
-          handleLogout()
+        if (error.toString().includes("403")) {
+          alert("O token expirou, favor logar novamente");
+          handleLogout();
         } else {
-          alert('Erro ao cadastrado a Categoria')
+          alert("Erro ao cadastrado a Categoria");
         }
       }
     }
 
-    retornar()
+    retornar();
   }
 
   function retornar() {
-    navigate("/categorias")
+    navigate("/categorias");
   }
 
   useEffect(() => {
-    if (token === '') {
-      alert('Você precisa estar logado');
-      navigate('/login');
+    if (token === "") {
+      alert("Você precisa estar logado");
+      navigate("/login");
     }
   }, [token]);
 
   return (
     <div className="container flex flex-col items-center justify-center mx-auto">
       <h1 className="text-4xl text-center my-8">
-        {categoriaId === undefined ? 'Cadastre uma nova Categoria' : 'Editar categoria'}
+        {categoriaId === undefined
+          ? "Editar categoria"
+          : "Cadastre uma nova Categoria"}
       </h1>
-
 
       <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
         <div className="flex flex-col gap-2">
-        <label htmlFor="nomeCategoria">Nome da Categoria</label>
+          <label htmlFor="nomeCategoria">Nome da Categoria</label>
           <input
             type="text"
             placeholder="Nome"
-            name='nomeCategoria'
+            name="nomeCategoria"
             className="border-2 border-slate-700 rounded p-2"
             value={categoria.nomeCategoria}
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
@@ -122,7 +118,7 @@ function FormularioCategoria() {
           <input
             type="text"
             placeholder="Descrição"
-            name='descricaoCategoria'
+            name="descricaoCategoria"
             className="border-2 border-slate-700 rounded p-2"
             value={categoria.descricaoCategoria}
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
@@ -132,7 +128,7 @@ function FormularioCategoria() {
           className="rounded text-slate-100 bg-indigo-400 hover:bg-indigo-800 w-1/2 py-2 mx-auto block"
           type="submit"
         >
-          {categoriaId === undefined ? 'Cadastrar' : 'Editar'}
+          {categoriaId === undefined ? "Editar" : "Cadastrar"}
         </button>
       </form>
     </div>

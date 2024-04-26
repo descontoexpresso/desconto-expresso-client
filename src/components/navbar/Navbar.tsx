512642ac7a873eb7a109b7adcf4043d9b50d3564
CajueiroCaio <caiovcajueiro@gmail.com>
2024-04-26 17:17:40 -0300

    feat alteração navbar, pagina perfil

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useContext } from "react";
-//import React from 'react'
 import logo from "../../assets/desconto-expresso-logo.png";
+import './Navbar.css';
 
 function Navbar() {
   const navigate = useNavigate();
-
   const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
@@ -25,34 +24,45 @@ function Navbar() {
           </div>
 
           <div className="flex gap-4 font-bold">
-            <Link
-              to="/login"
-              className="hover:underline hover:text-amber-600 transition-all duration-300"
-            >
-              Login
-            </Link>
-            <Link to="/home" className="hover:underline">
+            <Link to="/home" className="hover:text-verde-escuro transition-colors duration-400 ease-in-out transition-all duration-500">
               Home
             </Link>
-            <Link to="/categorias" className="hover:underline">
-              Categorias
+            <div className="relative dropdown">
+              <button className="hover:text-verde-escuro transition-colors duration-400 ease-in-out transition-all duration-500">
+                Categorias
+              </button>
+              <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 dropdown-content">
+                <div className="py-1 transition-colors duration-800 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <Link to="/categorias" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Todas as Categorias</Link>
+                  <Link to="/cadastrarCategoria" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Cadastrar Categorias</Link>
+                </div>
+              </div>
+            </div>
+            <div className="relative dropdown">
+              <button className="hover:text-verde-escuro transition-colors duration-400 ease-in-out transition-all duration-500">
+                Produtos
+              </button>
+              <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 dropdown-content">
+                <div className="py-1 transition-colors duration-800 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <Link to="/produtos" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Todos os Produtos</Link>
+                  <Link to="/cadastrarProduto" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Cadastrar Produtos</Link>
+                </div>
+              </div>
+            </div>
+            <Link to="/about" className="hover:text-verde-escuro transition-colors duration-400 ease-in-out transition-all duration-500">
+              Sobre Nós
             </Link>
-            <Link to="/cadastrarCategoria" className="hover:underline">
-              Cadastrar Categoria
-            </Link>
-            <Link to="/produtos" className="hover:underline">
-              Produtos
-            </Link>
-            <Link to="/cadastrarProduto" className="hover:underline">
-              Cadastrar Produto
-            </Link>
-            <Link to="/about" className="hover:underline">
-              About
-            </Link>
-            <Link to='/perfil' className='hover:underline'>Perfil</Link>
-            <Link to="/home" onClick={logout} className="hover:underline">
-              Sair
-            </Link>
+            <div className="relative dropdown">
+              <button>
+              <img src={usuario.fotoUsuario} alt="Foto do usuário" className="rounded-full w-8 h-8" />
+              </button>
+              <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 dropdown-content">
+                <div className="py-1 transition-colors duration-800 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <Link to="/perfil" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Perfil</Link>
+                  <Link to="/home" onClick={logout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Sair</Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
